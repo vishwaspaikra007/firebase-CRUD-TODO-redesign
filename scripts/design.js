030e112fd5138049a888f4852c4b4e0fb3489e08
Vishwas Paikra <vishwaspaikra007@gmail.com>
2019-05-16 13:37:39 +0530

    design for placeHolder pure js

---
+++ scripts/design.js
@@ -0,0 +1,14 @@
+// for place holder
+function placeHolder(a) {
+    placeHolder = document.querySelector(a);
+    placeHolder.style.top = "-2px";
+    placeHolder.style.fontSize = "20px";
+    placeHolder.style.width = "80%";
+    placeHolder.style.left = "4px";
+    placeHolder.style.opacity = "1";
+    setTimeout(() => {
+    placeHolder.style.textIndent = "20px";
+    }, 500);
+}
+
+//
