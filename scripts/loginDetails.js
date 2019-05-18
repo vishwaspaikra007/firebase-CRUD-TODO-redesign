e8f3a5c330d3b994e2128852d5bd65b966d97e2a
Vishwas Paikra <vishwaspaikra007@gmail.com>
2019-05-18 18:51:14 +0530

    Google signin with popup checking ux

--- scripts/loginDetails.js
+++ scripts/loginDetails.js
@@ -13,16 +13,12 @@
      loading();
     var provider = new firebase.auth.GoogleAuthProvider();
 
-    firebase.auth().signInWithRedirect(provider);
-
-    firebase.auth().getRedirectResult().then(function(result) {
-      if (result.credential) {
-        // This gives you a Google Access Token. You can use it to access the Google API.
-        var token = result.credential.accessToken;
-        // ...
-      }
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      var token = result.credential.accessToken;
       // The signed-in user info.
       var user = result.user;
+      // ...
     }).catch(function(error) {
       // Handle Errors here.
       var errorCode = error.code;
